22617733b1dc94a98c4a6056f5d7158e6d98d749
dnzgo <dnzgzc.98@gmail.com>
2023-05-30 21:08:29 +0300

    add whole project

---
+++ SnowBoarderPrototype/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+
+    bool hasCrashed = false;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground" && !hasCrashed)
+        {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ SnowBoarderPrototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+
+    Rigidbody2D rb2D;
+    SurfaceEffector2D surfaceEffector2D;
+
+    bool canMove = true;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+
+    void Update()
+    {
+        if(canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    public void DisableControls()
+    {
+        canMove = false;
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2D.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb2D.AddTorque(-torqueAmount);
+        }
+    }
+}
